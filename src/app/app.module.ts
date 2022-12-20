ebf8df0c9f816b7ef6a7a9201bf0e863b3b81e75
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2022-12-21 03:14:33 +0530

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
